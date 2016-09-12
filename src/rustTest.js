115cd3e47fa7d0770405a877191141cf5ff1a098
Arne Mæhlum <arne.mahlum@gmail.com>
2016-09-12 20:35:51 +0200

    Move the 'Hello Node' test code so we can make something useful

--- src/rustTest.js
+++ src/rustTest.js
@@ -1,9 +1,4 @@
-const ffi = require('ffi');
+// const ffi = require('ffi');
 
-const nativeRustFFI = ffi.Library('rust/target/release/libdecnalab', {
-    hello_from_rust: ['string', ['string']]
-});
-
-const string = nativeRustFFI.hello_from_rust('Node');
-
-console.log(string);
+// const nativeRustFFI = ffi.Library('rust/target/release/libdecnalab', {
+// });
