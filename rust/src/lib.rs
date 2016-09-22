6345aa8e1e230a20a6701c28a81ee63d9fc2b31e
Arne Mæhlum <arne.mahlum@gmail.com>
2016-09-22 19:59:06 +0200

    Send JSON to Rust (and get another JSON back)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,25 +6,27 @@ mod string_converter;
 
 // Automatically generate `RustcDecodable` and `RustcEncodable` trait implementations
 #[derive(RustcDecodable, RustcEncodable)]
-pub struct TestStruct  {
+pub struct TestStruct {
     data_int: u8,
     data_str: String,
     data_vector: Vec<u8>,
 }
 
 #[no_mangle]
-pub extern fn get_json() -> *const c_char {
+pub extern fn get_json(json_c_char: *const c_char) -> *const c_char {
+    let json_string: String = string_converter::to_string(json_c_char);
+
+    // Deserialize using `json::decode`
+    let decoded: TestStruct = json::decode(&json_string).unwrap();
+
     let object = TestStruct {
-        data_int: 1,
-        data_str: "homura".to_string(),
-        data_vector: vec![2,3,4,5],
+        data_int: decoded.data_int + 1,
+        data_str: format!("Hello {}", decoded.data_str).to_string(),
+        data_vector: vec![decoded.data_vector[0], 2, 3, 4, 5],
     };
 
     // Serialize using `json::encode`
     let encoded: String = json::encode(&object).unwrap();
-
-    // // Deserialize using `json::decode`
-    // let decoded: TestStruct = json::decode(&encoded).unwrap();
 
     string_converter::to_c_char(&encoded)
 }
