8cf47ada5d78b8a875171b0d75f8a768beae665d
Arne Mæhlum <arne.mahlum@gmail.com>
2016-09-24 17:42:59 +0200

    Implement methods on Vector
    - Test in main.rs
    
    - TODO: Create tests for Vector

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,46 @@ mod rts;
 use rts::spatial::vector::Vector;
 
 fn main() {
-    let vector = Vector{ x: 0, y: 5};
-
+    // create
+    let vector = Vector{ x: 10, y: 13 };
     println!("This is a vector: {:?}", vector);
+
+    // new()
+    let another_vector = Vector::new(1, 4);
+    println!("This is anoter vector: {:?}", another_vector);
+
+    // clone()
+    println!("This is a clone of the first vector: {:?}", vector.clone());
+
+    // zero()
+    println!("This is a zero length vector: {:?}", Vector::zero());
+
+    // add()
+    println!("This is the sum of two vectors: {:?}", vector.add(&another_vector));
+
+    // subtract()
+    println!("This is the difference between two vectors: {:?}", vector.subtract(&another_vector));
+
+    // scale()
+    println!("This is the first vector multiplied by 2: {:?}", vector.scale(2));
+
+    // absolute_length()
+    println!("This is the absolute length of a vector: {:?}", vector.absolute_length());
+
+    // to_unit_vector()
+    println!("This is the unit vector of vector: {:?}", vector.to_unit_vector());
+
+    // absolute_distance()
+    println!("This is the absolute distance between two points (vectors): {:?}", vector.absolute_distance(&another_vector));
+
+    // direction()
+    println!("This is the direction between two points (vectors): {:?}", vector.direction(&another_vector));
+
+    // is_equal_to()
+    println!("Is {:?} equal to {:?} ? {}", vector, another_vector, if vector.is_equal_to(&another_vector) { "Yes :)" } else { "No :(" });
+    println!("Is {:?} equal to {:?} ? {}", vector, vector.clone(), if vector.is_equal_to(&vector.clone()) { "Yes :)" } else { "No :(" });
+
+    // is_equal_to()
+    println!("Is {:?} different than {:?} ? {}", vector, another_vector, if vector.is_not_equal_to(&another_vector) { "Yes :)" } else { "No :(" });
+    println!("Is {:?} different than {:?} ? {}", vector, vector.clone(), if vector.is_not_equal_to(&vector.clone()) { "Yes :)" } else { "No :(" });
 }
--- rust/src/rts/spatial/vector.rs
+++ rust/src/rts/spatial/vector.rs
@@ -1,5 +1,71 @@
+use std::i32;
+
 #[derive(Debug)]
 pub struct Vector {
     pub x: i32,
     pub y: i32,
+}
+
+impl Vector {
+    pub fn new(x: i32, y: i32) -> Vector {
+        Vector{x: x, y: y}
+    }
+    pub fn clone(&self) -> Vector {
+        Vector::new(self.x, self.y)
+    }
+    pub fn zero() -> Vector {
+        Vector::new(0, 0)
+    }
+    pub fn add(&self, other: &Vector) -> Vector {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+    pub fn subtract(&self, other: &Vector) -> Vector {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+    pub fn scale(&self, scalar: i32) -> Vector {
+        Vector {
+            x: self.x * scalar,
+            y: self.y * scalar,
+        }
+    }
+
+    pub fn absolute_length(&self) -> i32 {
+        ((
+            (self.x).pow(2) +
+            (self.y).pow(2)
+        ) as f64).sqrt() as i32
+    }
+
+    pub fn to_unit_vector(&self) -> Vector {
+        let length = self.absolute_length();
+
+        self.clone().scale(1 / length)
+    }
+
+    // Positions
+    pub fn absolute_distance(&self, other: &Vector) -> i32 {
+        ((
+            (self.x - other.x).pow(2) +
+            (self.y - other.y).pow(2)
+        ) as f64).sqrt() as i32
+    }
+
+    // Positions
+    pub fn direction(&self, other: &Vector) -> Vector {
+        other.subtract(&self).to_unit_vector()
+    }
+
+    pub fn is_equal_to(&self, other: &Vector) -> bool {
+           self.x == other.x
+        && self.y == other.y
+    }
+    pub fn is_not_equal_to(&self, other: &Vector) -> bool {
+        !self.is_equal_to(other)
+    }
 }
