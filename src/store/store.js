85027fca3dcc0a149857fb03f11747ce9d3e4fb5
Arne Mæhlum <arne.mahlum@gmail.com>
2017-01-08 11:29:36 +0100

    Create store properly

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,18 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from 'reducers/rootReducer';
 
-export const createNewStore = () => createStore([ gameState ]);
+const crashReporter = store => next => action => {
+    try {
+        return next(action);
+    } catch (error) {
+        console.log('Action:', action, '\nState:', store.getState(), '\nError:', error.stack);
+
+        throw error;
+    }
+};
+
+const middleware = [thunk, crashReporter];
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+
+export const createNewStore = () => createStoreWithMiddleware(rootReducer);
