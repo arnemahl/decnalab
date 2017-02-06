54bdfa7ca46668eef9e9d076f3d094fcc36bdfca
Arne Mæhlum <arne.mahlum@gmail.com>
2017-02-06 19:04:04 +0100

    Coevolution: Log progress

--- src/coevolution/Coevolution.js
+++ src/coevolution/Coevolution.js
@@ -11,6 +11,8 @@ const maxGenerations = 8;
 const crossoverRatio = 0.95;
 const mutationRatio = 0.01;
 
+const DEBUG = true;
+const logProgress = DEBUG ? console.log : () => {};
 
 export function runCoevolution() {
     let generation = 0;
@@ -20,10 +22,12 @@ export function runCoevolution() {
     const initialPopulation = Array(nofChildrenPerGeneration).fill().map(Individual.generate);
 
     // select evaluators (teachSet)
+    logProgress('Creating initial Teach set...');
     let bestInHallOfFame = hallOfFame.slice(0, teachSetSize / 2);
     let teachSet = Individual.selectBySharedSampling(initialPopulation, hallOfFame, bestInHallOfFame, teachSetSize);
 
     // evaluate individuals from initialPopulation
+    logProgress('Evaluating initial population...');
     let wrappedPopulation = Individual.wrapWithSharedFitness(initialPopulation, teachSet);
 
     wrappedPopulation = selectUnique(wrappedPopulation, popSize, scaledFitnessSelection);
@@ -38,6 +42,7 @@ export function runCoevolution() {
         const parents = scaledFitnessSelection(wrappedPopulation, nofChildrenPerGeneration).map(Individual.unwrap);
 
         // produce children
+        logProgress('\nCreating childen...');
         const children = Array(nofChildrenPerGeneration / 2).fill().map(() => {
             // Crossover
             const mother = parents.pop();
@@ -59,10 +64,12 @@ export function runCoevolution() {
         });
 
         // update evaluators (teachSet)
+        logProgress('Updating Teach set...');
         bestInHallOfFame = Individual.selectBySharedSampling(hallOfFame, teachSet, [], teachSetSize / 2);
         teachSet = Individual.selectBySharedSampling(population, teachSet, bestInHallOfFame, teachSetSize);
 
         // evaluate children
+        logProgress('Evaluating children...');
         const wrappedChildren = Individual.wrapWithSharedFitness(children, teachSet);
 
         // select survivors for next generation
@@ -72,6 +79,7 @@ export function runCoevolution() {
         population = wrappedPopulation.map(Individual.unwrap);
 
         // add best individual from generation
+        logProgress('Adding best individual to Hall of Fame...');
         hallOfFame.push(Individual.getBestAdditionToSample(population, teachSet, hallOfFame));
     }
 
