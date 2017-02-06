0bec30486dedc95ec987ab8cd499191b379a0966
Arne Mæhlum <arne.mahlum@gmail.com>
2017-02-06 15:58:13 +0100

    Add dump file examples

---
+++ dump/ai-config/example.js
@@ -0,0 +1,102 @@
+module.exports = [
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 20
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 4
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 20
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 4
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            }
+        ],
+        "attackAtSupply": 20
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            }
+        ],
+        "attackAtSupply": 20
+    }
+];
