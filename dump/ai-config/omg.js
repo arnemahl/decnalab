f4e994c1a4354c776061760856285756ca0e6f78
Arne Mæhlum <arne.mahlum@gmail.com>
2017-02-07 03:22:58 +0100

    Epic game

---
+++ dump/ai-config/omg.js
@@ -0,0 +1,710 @@
+module.exports = [
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 4
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 4
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 4
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 4
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "Barracks",
+                "addCount": 1
+            },
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "SupplyDepot",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 3
+            }
+        ],
+        "attackAtSupply": 16
+    },
+    {
+        "buildOrder": [
+            {
+                "specName": "Worker",
+                "addCount": 1
+            },
+            {
+                "specName": "Marine",
+                "addCount": 2
+            }
+        ],
+        "attackAtSupply": 16
+    }
+];
