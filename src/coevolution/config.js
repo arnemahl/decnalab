e7f15bc4dcd1d9ecb81462302b621ca63b0ebe39
Arne Mæhlum <arne.mahlum@gmail.com>
2017-03-02 22:41:53 +0100

    Increase crossoverRatio (0.90 -> 0.95)

--- src/coevolution/config.js
+++ src/coevolution/config.js
@@ -4,7 +4,7 @@ export const teachSetSize = 8;
 export const maxGenerations = 50;
 export const maxLoopsPerGame = 1000;
 export const fitnessScalingFactor = 1.5;
-export const crossoverRatio = 0.90;
+export const crossoverRatio = 0.95;
 export const perBitMutationRatio = 0.01;
 
 export const crossoverFunction = 'uniformCrossover';
