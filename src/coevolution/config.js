27b8d1ac9670eba12a4f854dfb6169d98d06364b
Arne Mæhlum <arne.mahlum@gmail.com>
2017-03-03 10:51:27 +0100

    Add explanation of constants in CA config

--- src/coevolution/config.js
+++ src/coevolution/config.js
@@ -21,3 +21,118 @@ export const producableThings = [
 export const minAttackTiming = 6;
 export const maxAttackTiming = 42;
 export const initialBuildOrderLength = 8;
+
+
+/*
+
+=========================
+Explanation of Constants:
+=========================
+
+# popSize
+
+    Size of a population at the start of a generation
+
+
+# nofChildrenPerGeneration
+
+    How many new individuals that are created per generation. Must be equal to or larger
+    than popSize. If larger, some children will 'die' before next generation starts.
+
+# teachSetSize
+
+    The teach set contains individuals that other individuals are evaluated against.
+    All created individuals will play against all the members in the current teach set.
+
+    Having a large teachSet provides better evaluation, at the cost of run-time
+    performance.
+
+
+# maxGenerations
+
+    How many generations to run the coevolutionary algorithm for.
+
+
+# maxLoopsPerGame
+
+    Maximum number of loops per game.
+
+
+# fitnessScalingFactor
+
+    Used to scale fitness.
+
+    0 -> All individuals have equal probability of being selected
+    1 -> Best individual has 2x probability of being selected compared to individual with fitness 0
+    Approaching infinity -> Approaching no scaling effect
+
+    Using scaled fitness increses the chance of preserving genetic diversity, by
+    increasing probability of less fit indiviudals to be selected.
+
+
+# crossoverRatio
+
+    Probability of crossover. When there is no crossover, the children of two parents
+    are identical to the parents (unless they are mutated).
+
+
+# crossoverPointRatio
+
+    Probability per gene of adding a crossover point between it and the previous gene.
+    Lower `crossoverPointRatio` means fewer crossover points. Between each crossover point,
+    a child will receive genes from the same parent.
+
+    0 -> no crossover points
+    0.5 -> works just as if we didn't have this variable
+    1 -> crossover point between every single gene
+
+    Setting a value below 0.5 we get a higher probability of transferring the full meaning
+    of a gene sequence from parent to child. Example:
+
+        Gene representation of targets:
+            00 -> Worker
+            01 -> Marine
+            10 -> SupplyDepot
+            11 -> Barracks
+
+        If during crossover we cut in the middle of a gene sequence encoding for a target,
+        for which mother contains Worker (00) and father contains Barracks (11), children
+        will get Marine (01) and SupplyDepot (10), thus they don't get the targets of any
+        of the parents.
+
+        By setting `crossoverPointRatio` below 0.5 we reduce the chance of cutting
+        in the middle of a gene, and increase the chance that the targets of the parents
+        are transferred to the children.
+
+# perBitMutationRatio
+
+    Probability for each individual bit in the genome to be flipped (0 into 1, 1 into 0).
+
+
+# crossoverFunction
+
+    Which function to use for crossover, creating two children from two parents.
+
+
+# producableThings
+
+    Units/structures that can be part of the build order and thus produced by the AI.
+
+
+# minAttackTiming
+
+    Lowest possible value for `attackAtSupply`. When an AI's has enough units that used
+    supply is bigger than or equal to `attackAtSupply`, the AI will launch an attack on the
+    enemy.
+
+
+# maxAttackTiming
+
+    Highest possible value for `attackAtSupply`. See `minAttackTiming`.
+
+
+# initialBuildOrderLength
+
+    The length of the build order encoded in the genome, that the AI will attempt to execute.
+
+*/
