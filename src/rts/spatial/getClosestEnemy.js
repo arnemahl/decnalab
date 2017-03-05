5373c07e02ddb18b818db89373aad985e76514aa
Arne Mæhlum <arne.mahlum@gmail.com>
2017-03-05 18:11:04 +0100

    DumbAI: Smarter micro
    - Clean up duplicated-ish code
    - If unit is in range of enemy unit, prioritize
      attacking units over structures
    - Abort attack when falling below attackAtSupply

--- src/rts/spatial/getClosestEnemy.js
+++ src/rts/spatial/getClosestEnemy.js
@@ -2,6 +2,12 @@ import Vectors from '~/rts/spatial/Vectors';
 
 export const byClosenessTo = (position) => (one, two) => Vectors.absoluteDistance(position, one.position) - Vectors.absoluteDistance(position, two.position);
 
+export const getClosestEnemyUnitWithinRange = (unit) => unit.team.visibleEnemyCommandables
+    .filter(commandable => commandable.type === 'unit')
+    .filter(enemyUnit => Vectors.absoluteDistance(unit.position, enemyUnit.position) <= unit.specs.weapon.range)
+    .sort(byClosenessTo(unit.position))
+    [0];
+
 const getClosestEnemy = (unit) => unit.team.visibleEnemyCommandables.sort(byClosenessTo(unit.position))[0];
 
 export default getClosestEnemy;
